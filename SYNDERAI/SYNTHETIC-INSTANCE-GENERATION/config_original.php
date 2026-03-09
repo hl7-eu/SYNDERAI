13344d815d4780a83ae60dddb41a7619b65705b6
dr Kai U. Heitmann <info@kheitmann.de>
2026-03-09 21:12:41 +0100

    Virgin config template

---
+++ SYNDERAI/SYNTHETIC-INSTANCE-GENERATION/config_original.php
@@ -0,0 +1,13 @@
+<?php
+
+/* debug or not debug that is the question */
+define("DEBUGLEVEL", 2);
+
+/* use of AI */
+define("USE_AI",                   TRUE);
+// define("OPEN_AI_API_KEY",       "...");
+define("OPEN_AI_API_KEY",          "...");
+// define API key for Claude
+define("ANTHROPIC_API_KEY",        "...");
+
+?>
