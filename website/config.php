bfac2639f18e70a023100cc43e680fcc8e769f9a
dr Kai U. Heitmann <info@kheitmann.de>
2026-03-17 09:57:42 +0100

    Added examples, website config

---
+++ website/config.php
@@ -0,0 +1,51 @@
+<?php
+/*
+    Define the top menu structure
+*/
+$MENU = [
+    [
+        "title" => "Home",
+        "menu" => "index",
+        "file" => ""
+    ],
+    [
+        "title" => "The Story",
+        "menu" => "story",
+        "file" => "STORY.md"
+    ],
+    [
+        "title" => "Principles",
+        "menu" => "principles",
+        "file" => "PRINCIPLES.md"
+    ],
+    [
+        "title" => "Progress",
+        "menu" => "progress",
+        "file" => "PROGRESS.md"
+    ],
+    [
+        "title" => "Personas",
+        "menu" => "personas",
+        "file" => "PERSONAS.md"
+    ],
+    [
+        "title" => "Examples",
+        "menu" => "examples",
+        "file" => "-"
+    ],
+    [
+        "title" => "Policy",
+        "menu" => "policy",
+        "file" => "SYNDERAI-SYNTHETIC-DATA-POLICY.md"
+    ],
+    [
+        "title" => "Credits+",
+        "menu" => "credits",
+        "file" => "CCC.md"
+    ]
+];
+/*
+    vi7eti integration – deep link
+*/
+$VI7ETIDEEPLINK = "https://vi7eti.net/index.php?";
+$SELFURL = "https://synderai.net";
